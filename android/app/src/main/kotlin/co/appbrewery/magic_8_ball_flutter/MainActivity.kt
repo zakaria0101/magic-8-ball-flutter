86c8ce42ccc50d9250552e6df9c8082137532b5f
zakaria0101 <zraissi403@gmail.com>
2024-01-23 15:49:22 +0100

    last

---
+++ android/app/src/main/kotlin/co/appbrewery/magic_8_ball_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package co.appbrewery.magic_8_ball_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Ball(),
+    ),
+  );
+}
+
+class Ball extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[800],
+        title: Center(
+          child: Text('Ask Me Anything'),
+        ),
+      ),
+      body: MagicBall(),
+    );
+  }
+}
+
+class MagicBall extends StatefulWidget {
+  @override
+  _MagicBallState createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int rd = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+        onPressed: () {
+          setState(() {
+            rd = Random().nextInt(5) + 1;
+          });
+        },
+        child: Image.asset('images/ball$rd.png'),
       ),
     );
+  }
+}
